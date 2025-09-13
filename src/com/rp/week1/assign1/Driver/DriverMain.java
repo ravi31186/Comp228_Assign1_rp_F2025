502352ae113407c2fdadcb7956e6da81ce8dcb71
ravi31186 <ravi31186@gmail.com>
2025-09-13 14:44:09 -0400

    created assignment structure as per instruction.

---
+++ src/com/rp/week1/assign1/Driver/DriverMain.java
@@ -0,0 +1,10 @@
+package com.rp.week1.assign1.Driver;
+
+public class DriverMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/rp/week1/assign1/GradeCalculation.java
@@ -0,0 +1,5 @@
+package com.rp.week1.assign1;
+
+public class GradeCalculation {
+
+}
